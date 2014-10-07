d7d78727da98350a4d346a27a021e3d4333cdfa0
tarzelus <zubiri@Ubuntu-12-04.(none)>
2014-10-07 09:37:12 +0200

    aa

---
+++ zubiri/geometria/Circulo.java
@@ -0,0 +1,40 @@
+//para agrupar en el paquete y poder usar las clases de este fichero en "operacionesGeometricas"
+package com.zubiri.geometria;
+
+/*
+ * clase para calcular la circunferencia y el area del circulo
+ */
+
+public class Circulo {
+
+double radio1=0;
+double result;
+ //Método para calcular la circunferencia del circulo
+ public double circun () {
+
+	double result = 0;
+	result = 6.283185307179586476925286766559 * getRadio();
+	return result;
+ }
+
+
+
+ //Método para calcular el area del circulo mediante getRadio
+ public double area () {
+
+	double result = 0;
+	result = 3.1415926535897932384626433832795 * getRadio()* getRadio();
+	return result;
+ }
+
+//declaracion de los get y set
+private double getRadio(){
+	return radio1;
+}
+public void  setRadio(double radio){
+
+radio1=radio;
+}
+
+}
+
---
+++ zubiri/geometria/Cuadrado.java
@@ -0,0 +1,77 @@
+//para agrupar en el paquete y poder usar las clases de este fichero en "operacionesGeometricas"
+package com.zubiri.geometria;
+
+public class Cuadrado {
+
+	static final double NUM_PI = 3.1416;
+
+	private double alto1 = 0;
+	private double ancho1 = 0;
+	private double result;	  
+	private double area;
+	private double perimetro;
+
+//metodo para calcular el perimetro del cuadrado
+	public double perimetro() {
+
+		double resultadoPerimetro;
+
+		resultadoPerimetro = 2*this.getAlto()+ 2* this.getAncho();
+		return resultadoPerimetro;
+	}
+
+// Metrodo para calcular erl area del cuadrado
+	public double area() {
+
+		double resultadoArea;
+
+		resultadoArea =this.getAlto()*this.getAncho();
+
+		return resultadoArea;
+	}
+//definiendo los get y set
+
+	public double getAlto() {
+
+		return alto1;
+	}	
+
+	public void setAlto(double alto) {
+
+		alto1 = alto;
+	} 
+
+        public double getAncho() {
+
+		return ancho1;
+	}	
+
+	public void setAncho(double ancho) {
+
+		ancho1 = ancho;
+	} 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
